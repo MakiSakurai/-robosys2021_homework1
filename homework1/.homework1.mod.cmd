e62b5ac4f36a0a32a2972fcb8dafc28f8d8d46b3
MakiSakurai <nikumann8@gmail.com>
2021-12-16 10:57:03 +0900

    makefileで作成されたものを消去

--- homework1/.homework1.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ubuntu/homework1/homework1.mod := { echo  /home/ubuntu/homework1/homework1.o;  echo; } > /home/ubuntu/homework1/homework1.mod
--- homework1/homework1.mod
+++
@@ -1,2 +0,0 @@
-/home/ubuntu/homework1/homework1.o
-
